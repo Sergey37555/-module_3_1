3d2fe61b4ed692c254438a0850d36775b30cd402
Sergey37555 <24twister555@mail.ru>
2024-08-04 16:42:05 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,34 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    stroka = str(string)
+    result = (len(stroka), stroka.upper(), stroka.lower())
+    count_calls()
+    return result
+
+
+def is_contains(string, list_to_search):
+    string = str(string).lower()
+    list_to_search = list(list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i]).lower() == string:
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+
+
+print(string_info('Привет'))
+print(string_info('Пока'))
+print(is_contains('яблоко', ['апельсин', 'банан', ]))
+print(is_contains('машина', ['дача', 'квартира', 'машина']))
+print(calls)
